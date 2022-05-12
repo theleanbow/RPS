db25dca9cbad3092decb8cc051f4b9b952ff420d
LeanBow <theleanbow@gmail.com>
2022-05-12 17:13:03 +0545

    Works partially

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-console.log("Welcome to Rock, Papers, Scissors Console-based Game!!!");
-
 //generate rock, paper, scissors randomly
 function computerPlay() {
-  choices = ["rock", "paper", "scissors"];
+  choices = ["ROCK", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
 
@@ -10,19 +8,14 @@ function computerPlay() {
 function playRound(playerSelect, computerSelection) {
   playerSelection = playerSelect.toLowerCase();
   if (playerSelection === computerSelection) {
-    // return `Its a draw!!   ${playerSelection} vs ${computerSelection}`;
     return "draw";
   } else if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "rock")
   ) {
-    // console.log(`You lose!! ${computerSelection} beats ${playerSelection}`);
-    // return `You lose!! ${computerSelection} beats ${playerSelection}`;
     return "lose";
   } else {
-    // console.log(`You win!! ${playerSelection} beats ${computerSelection}`);
-    //   return `You win!! ${playerSelection} beats ${computerSelection}`;
     return "win";
   }
 }
@@ -31,23 +24,26 @@ let player = 0;
 const computerSelection = computerPlay();
 const container = document.querySelector(".container");
 const div = document.createElement("div");
+const scoreCards = document.createElement("div");
 const buttons = document.querySelectorAll(".buttons");
 const button = document.querySelector(".buttons");
 buttons.forEach((button) => {
   const playerSelection = button.innerHTML;
   button.addEventListener("click", function returned() {
     value = playRound(playerSelection, computerSelection);
-    div.innerText = value;
-    container.appendChild(div);
-    if (value === "win") computer++;
-    if (value === "lose") player++;
-    const result = document.createElement("div");
-    result.innerText = `${computer} vs ${player}`;
-    div.appendChild(result);
+    div.innerText = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`;
+    if (player < 5 && computer < 5) {
+      container.appendChild(div);
+      if (value === "win") computer++;
+      else if (value === "lose") player++;
+    }
+    const scoreCards = document.createElement("div");
+    scoreCards.innerText = `${computer} vs ${player}`;
+    div.appendChild(scoreCards);
     if (player === 5 || computer === 5) {
       const winner = document.createElement("div");
       winner.innerText = `GameOver`;
-      result.appendChild(winner);
+      scoreCards.appendChild(winner);
     }
   });
 });
